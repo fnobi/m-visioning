d7d6d0066d884d58dec43842f7ae5c5575970bd0
fnobi <fnobi0.8@gmail.com>
2025-10-18 15:48:02 +0900

    ログインをひっすにしていくう

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,20 @@
+import MockLoadingScene from "~/common/components/MockLoadingScene";
+import { useAuthorizedUser } from "~/common/lib/firebase-auth-tools";
+import LoginScene from "~/app/components/LoginScene";
 import MVisionTopScene from "~/app/components/MVisionTopScene";
 
-const PageIndex = () => <MVisionTopScene />;
+const PageIndex = () => {
+  const { myId, isAuthLoading } = useAuthorizedUser();
+
+  if (isAuthLoading) {
+    return <MockLoadingScene />;
+  }
+
+  if (!myId) {
+    return <LoginScene />;
+  }
+
+  return <MVisionTopScene />;
+};
 
 export default PageIndex;
